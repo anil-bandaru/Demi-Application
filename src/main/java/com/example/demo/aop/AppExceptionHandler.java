84b8b010a46515d4bf6fc5e71140c6af586b4277
anil-bandaru <100435727+anil-bandaru@users.noreply.github.com>
2022-02-25 23:44:38 +0530

    aop class updated

--- src/main/java/com/example/demo/aop/AppExceptionHandler.java
+++ src/main/java/com/example/demo/aop/AppExceptionHandler.java
@@ -1,15 +1,4 @@
-/*  
-*  
-* Copyright (c) 2020 OLAM Limited  
-*  
-* All information contained herein is, and remains the property of OLAM  
-* Limited. The intellectual and technical concepts contained herein are  
-* proprietary to OLAM and are protected by trade secret or copyright law.  
-* Dissemination of this information or reproduction of this material is  
-* strictly forbidden unless prior written permission is obtained from OLAM  
-* Limited  
-*  
-*/  
+ 
 
 package com.example.demo.aop;
 
@@ -21,10 +10,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.example.demo.exception.AppErrorResponse;
 
 
-/**
- * Class for Handling all the AppException
- * @version 1.0
- */
 @RestControllerAdvice
 public class AppExceptionHandler {
 
